ca13eeb1ba4fc5cee01fe6999257320f74dd7219
wingo1990 <wingo1990@163.com>
2014-09-29 22:32:42 +0800

    解决显示不同的问题

--- CppStudy/01cpp.cpp
+++ CppStudy/01cpp.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 int main(void)
 {
-	const int a = 100;
+	const volatile int a = 100;
 
 	int *p = const_cast<int*>(&a);
 
 	*p = 200;
 
-	cout << "&a = "<<&a<<endl;
-	cout << "p = " <<p<<endl;
+//	cout << "&a = "<<&a<<endl;
+//	cout << "p = " <<p<<endl;
 
-	cout <<"a = "<<a<<endl;
+	cout <<"a1 = "<<a<<endl;				//a 用volatile 修饰打印的就一样了？
+	cout <<"a2 = "<<*(int*)&a<<endl;
 	cout <<"*p = "<<*p<<endl;
+
+	//下面这个例子是合理的，而上面的则是不合理的，其具体原因是：
+	//const_cast其要转换的对象时指针和引用才有意义。
+
+	const int *b = new int(100);
+	int *q = const_cast<int*>(b);
+
+	*q = 200;
+	cout << b << q <<endl;
+	cout <<*b <<*q <<endl;
+
 
 	return 0;
 }
