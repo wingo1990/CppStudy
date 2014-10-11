a92428f8db8bd2a02888e8a535e108929c8f2205
wingo1990 <wingo1990@163.com>
2014-10-11 13:30:22 +0800

    常量折叠巩固

--- 01cpp/01cpp.cpp
+++ 01cpp/01cpp.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 int main(void)
 {
-	const volatile int a = 100;				// 在没有volatile的时候，C++采用的优化，用到a替换成100
+	volatile const int a = 100;				// 在没有volatile的时候，C++采用的优化，用到a替换成100
 
 	int *p = const_cast<int*>(&a);
 
 	*p = 200;
 
-	cout << "&a = "<<&a<<endl;
+	cout << "&a = "<<(int*)&a<<endl;
 	cout << "p = " <<p<<endl;
 
-	cout <<"a1 = "<<a<<endl;				//a 用volatile 修饰打印的就一样了？
+	cout <<"a1 = "<<a<<endl;				//a 用volatile 修饰打印的就一样了,可以保证a是存内存中读，不是从符号表中读取。
 	cout <<"a2 = "<<*(int*)&a<<endl;
 	cout <<"*p = "<<*p<<endl;
 
 	//下面这个例子是合理的，而上面的则是不合理的，其具体原因是：
 	//const_cast其要转换的对象时指针和引用才有意义。
 
+	//指针
 	const int *b = new int(100);
 	int *q = const_cast<int*>(b);
 
@@ -29,5 +30,47 @@ int main(void)
 	cout <<"*b="<<*b <<endl;
 	cout <<"*q="<<*q <<endl;
 
+	//引用
+	volatile const int i = 100;
+	int &j = const_cast<int &>(i);
+	cout << "before &i = " << (int*)&i <<endl;
+	cout << "befoer &j = " << &j <<endl;
+
+	j = 200;
+
+	cout << "after &i = " << (int*)&i <<endl;
+	cout << "after &j = " << &j <<endl;
+
+	cout << "i = " << i << endl;
+	cout << "j = " << j << endl;
+ 
+
+
+	//自定义
+	class MyClass
+	{
+	public:
+		int a;
+		double b;
+		MyClass(int a1, double b1): a(a1), b(b1){}
+	};
+
+	const MyClass aa(1,2.5);
+	MyClass &bb = const_cast<MyClass &>(aa);
+
+	cout<< "before &aa = " << &aa << endl;
+	cout<< "before &bb = " << &bb << endl;
+
+	bb.a = 100;
+	bb.b = 25.5;
+
+	cout<< "after &aa = " << &aa << endl;
+	cout<< "after &bb = " << &bb << endl;
+
+	cout << "aa.a = " << aa.a << endl;
+	cout << "aa.b = " << aa.b << endl;
+	cout << "bb.a = " << bb.a << endl;
+	cout << "bb.b = " << bb.b << endl;
+	
 	return 0;
 }
